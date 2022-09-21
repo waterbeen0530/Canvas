2af736ce3227b6fa56e26f54faff68c0dacf253e
waterbeen0530 <hange253@gmail.com>
2022-09-21 21:52:51 +0900

    [CREATE] Canvas

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+
+function Canvas() {
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [ctx, setCtx] = useState(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 0.55;
+    canvas.height = window.innerHeight * 0.8;
+
+    const context = canvas.getContext("2d");
+    context.strokeStyeld = "black";
+    context.lineWidth = 2.5;
+    contextRef.current = context;
+
+    setCtx(context);
+  }, []);
+
+  const startDrawing = () => {
+    setIsDrawing(true);
+  };
+
+  const finishDrawing = () => {
+    setIsDrawing(false);
+  };
+
+  const drawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+    if (ctx) {
+      if (!isDrawing) {
+        ctx.beginPath();
+        ctx.moveTo(offsetX, offsetY);
+      } else {
+        ctx.lineTo(offsetX, offsetY);
+        ctx.stroke();
+      }
+    }
+  };
+
+  return (
+    <Container>
+      <CanvasSection
+        ref={canvasRef}
+        onMouseDown={startDrawing}
+        onMouseUp={finishDrawing}
+        onMouseMove={drawing}
+        onMouseLeave={finishDrawing}
+      ></CanvasSection>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #e1e2e3;
+`;
+
+const CanvasSection = styled.canvas`
+  background: #fff;
+`;
+
+export default Canvas;
